f5497467219876ed16ed9c239a903c0005cabaf2
626 <nurjigitumarov22@gmail.com>
2023-07-19 19:39:31 +0300

    double

--- src/Circle.java
+++ src/Circle.java
@@ -1,11 +1,9 @@
 public class Circle {
     private static final double Pi = 3.114;
-    public Circle() {
-    }
-    public static double area(int radius) {
+    public static double area(double radius) {
         return Pi * (radius * radius);
     }
-    public static double circumference(int radius) {
+    public static double circumference(double radius) {
         return Pi * 2 * radius;
     }
 
